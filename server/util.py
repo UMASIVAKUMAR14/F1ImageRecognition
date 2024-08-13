5b30e52ffec77f8f7c8948d119241219e4207332
UMASIVAKUMAR14 <102385174+UMASIVAKUMAR14@users.noreply.github.com>
2024-08-13 16:22:20 -0500

    remove first return of get_cropped_image to ensure a face is returned

--- server/util.py
+++ server/util.py
@@ -40,20 +40,13 @@ def get_cropped_image(image_path, image_base64_data):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face.detectMultiScale(gray, 1.3, 5)
     
-    print("faces : ", end='')
-    print(faces)
-    
     cropped_faces = []
     for (x, y, w, h) in faces:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         eyes = eye.detectMultiScale(roi_gray)
-        if len(eyes) >= 2:
-            print("cropped : ", end='')
-            print(cropped_faces)
-            return cropped_faces.append(roi_color)
-    print("not cropped : ", end='')
-    print(cropped_faces)
+        cropped_faces.append(roi_color)
+    
     return cropped_faces
 
 
@@ -67,11 +60,8 @@ def classify_image(image_base64_data, file_path=None):
 
     imgs = get_cropped_image(file_path, image_base64_data)
     
-    if imgs is None:
-        return "get_cropped_image returned None."
-    
-    print("imgs : ", end='')
-    print(imgs)
+    if (imgs == []):
+        return "Make sure the image clearly shows a face and both eyes."
 
     result = []
     for img in imgs:
@@ -82,11 +72,12 @@ def classify_image(image_base64_data, file_path=None):
 
         len_image_array = 32*32*3 + 32*32
         final = combined_img.reshape(1,len_image_array).astype(float) 
-        result.append({
-            'class': class_number_to_name(__model.predict(final)[0]),
-            'class_probability': np.around(__model.predict_proba(final)*100,2).tolist()[0],
-            'class_dictionary': __class_name_to_number
-        })
+        result.append(class_number_to_name(__model.predict(final)[0]))
+        # result.append({
+        #     'class': class_number_to_name(__model.predict(final)[0]),
+        #     'class_probability': np.around(__model.predict_proba(final)*100,2).tolist()[0],
+        #     'class_dictionary': __class_name_to_number
+        # })
 
     return result
 
